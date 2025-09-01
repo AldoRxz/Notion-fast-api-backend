329d7510bd47ed49a55f026d7e71d5681f6f882c
Aldo <aldo_roman13@hotmail.com>
2025-08-31 20:46:16 -0600

    feat(workspaces): add GET endpoint in router

--- app/workspaces/router.py
+++ app/workspaces/router.py
@@ -9,3 +9,7 @@ router = APIRouter(prefix="/workspaces", tags=["workspaces_v2"])
 @router.post("/", response_model=schemas.WorkspaceRead)
 async def create(dto: schemas.WorkspaceCreateIn, user_id: uuid.UUID = Depends(get_current_user_id), uow: SqlAlchemyUoW = Depends(get_uow)):
     return await services.create_workspace(uow, user_id, dto)
+
+@router.get("/", response_model=list[schemas.WorkspaceRead])
+async def list_my_workspaces(user_id: uuid.UUID = Depends(get_current_user_id), uow: SqlAlchemyUoW = Depends(get_uow)):
+    return await services.list_workspaces(uow, user_id)
